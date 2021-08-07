2bc58c70e84f62915af4ed0b9950b1f89ea63417
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-08-07 15:09:32 -0700

    Update sum-of-all-odd-length-subarrays.py
    Added a more optimal solution to the problem

--- sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
+++ sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
@@ -1,5 +1,5 @@
 class Solution:
-    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+    def sumOddLengthSubarraysBrute(self, arr: List[int]) -> int:
         '''
         Returns the sum of all odd-length subarrays in the given array
         Returns the value as an int
@@ -8,4 +8,16 @@ class Solution:
         for i in range(len(arr)):
             for j in range(i, len(arr), 2):
                 result += sum(arr[i:j+1])
+        return result
+    
+    def sumOddLengthSubarrays(arr: list[int]) -> int:
+        '''
+        Returns the sum of all odd-length subarrays in the given array
+        Returns the value as an int
+        '''
+        result = 0
+        for i in range(0, len(arr)):
+            total = (len(arr) - i) * (i + 1)
+            odd = (total + 1) // 2
+            result += odd * arr[i]
         return result

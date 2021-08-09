8a83bec700aef1f202216d224f81f27eff06ba6f
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-08-09 03:40:07 -0700

    Cleaned up the function

--- excel-sheet-column-number/excel-sheet-column-number.py
+++ excel-sheet-column-number/excel-sheet-column-number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
+    def titleToNumberUgly(self, columnTitle: str) -> int:
         '''
         Given a string of chars in range A-Z in any combination, calculates the value
         Returns the sum as an int
@@ -10,4 +10,10 @@ class Solution:
         for i in range(len(val_list)-1, -1, -1): #Calculates Base26 value of the number
             sum += val_list[i] * loc
             loc *= 26
+        return sum
+    
+    def titleToNumber(columnTitle: str) -> int: #Cleaned up function
+        sum = 0
+        for c in columnTitle:
+            sum = sum * 26 + (ord(c) - 64)
         return sum

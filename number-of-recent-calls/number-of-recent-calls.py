613bd4dcfdd44ad0c518d739310d5d57060cf500
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-08-08 21:06:29 -0700

    Update number-of-recent-calls.py
    Added a more optimal solution

--- number-of-recent-calls/number-of-recent-calls.py
+++ number-of-recent-calls/number-of-recent-calls.py
@@ -1,9 +1,12 @@
+from typing import Deque
+
 class RecentCounter:
 
     def __init__(self):
         self.counter = []
+        self.window = Deque()
 
-    def ping(self, t: int) -> int:
+    def pingBrute(self, t: int) -> int:
         pings = 0
         self.counter.append(t)
         for i in range(len(self.counter), 0, -1):
@@ -13,6 +16,11 @@ class RecentCounter:
                 break
         return pings
 
+    def ping(self, t: int) -> int:
+        self.window.append(t)
+        while self.window[0] < t - 3000:
+            self.window.popleft()
+        return len(self.window)
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()

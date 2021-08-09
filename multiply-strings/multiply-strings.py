4f8b1e8a02bdfb71e0747f8034430d72976b2ee9
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-08-09 14:09:37 -0700

    Added a more complete answer

--- multiply-strings/multiply-strings.py
+++ multiply-strings/multiply-strings.py
@@ -1,11 +1,22 @@
 class Solution:
+    def multiplyEasy(num1: str, num2: str):
+        '''
+        Easiest possible solution to the problem
+        But breaks all the rules
+        '''
+        return str(int(num1) * int(num2))
+    
     def multiply(self, num1: str, num2: str) -> str:
+        '''
+        A more expanded solution to the problem without breaking rules
+        Converts the numbers to integer by hand and converts them back to string
+        '''
         sum1 = 0
         sum2 = 0
         for c in num1: #Convert num1 to int
             sum1 *= 10
-            sum1 += int(c)
+            sum1 += ord(c) - 48
         for c in num2: #Convert num2 to int
             sum2 *= 10
-            sum2 += int(c)
+            sum2 += ord(c) - 48
         return str(sum1 * sum2)

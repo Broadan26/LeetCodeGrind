4b8957fbb02c8b4eee004f92e8e3fc1bfa99bc21
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-08-15 01:43:14 -0700

    Added some more explanation

--- nim-game/nim-game.py
+++ nim-game/nim-game.py
@@ -1,3 +1,8 @@
 class Solution:
-    def canWinNim(self, n: int) -> bool:
-        return (n % 4) != 0
+    def canWinNim(self, n: int) -> bool: 
+        '''
+        Determines if the player who goes first can win the game of nim
+        Players takes 1-3 stones each time
+        Returns true if player going first can win, otherwise false
+        '''
+        return (n % 4) != 0 #Player going first just needs to avoid a multiple of 4 to win

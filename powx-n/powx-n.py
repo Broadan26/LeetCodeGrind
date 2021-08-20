fdc8f28a54683c5b944af5132a119a428bc832a4
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-08-20 08:16:35 -0700

    Time: 20 ms (99.12%), Space: 14.2 MB (50.90%) - LeetHub

---
+++ powx-n/powx-n.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        '''
+        Solving the problem as intended using recursion
+        Slower than average for this reason
+        '''
+        solution = 1.0
+        power = n
+        if n < 0:
+            power *= -1
+        while power:
+            if power % 2:
+                solution *= x
+                power -= 1
+            else:
+                x *= x
+                power /= 2
+        if n < 0:
+            solution = 1.0 / solution
+        return solution

d4c6a274e2ff56d49caf7e79b79fd95e122f58e9
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-09-09 01:44:32 -0700

    Cleaned up code

--- summary-ranges/summary-ranges.py
+++ summary-ranges/summary-ranges.py
@@ -1,21 +1,14 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         ranges = []
-        if len(nums) == 0: #nums is empty
-            return ranges
-
-        start = nums[0]
-        prev = nums[0]
-        for i in range(0, len(nums)):
-            if start == prev and nums[i] > prev + 1: #Range is length 1
-                ranges.append(str(start))
-                start = nums[i]
-            elif prev + 1 < nums[i]: #Range spans multiple digits
-                ranges.append(str(start) + '->' + str(prev))
-                start = nums[i]
-            if prev + 1 == nums[i] and i + 1 == len(nums): #Range spans multiple digits, last iteration
+        i = 0
+        while i < len(nums): #Handles the start of ranges
+            start = nums[i]
+            while i + 1 < len(nums) and nums[i+1] == nums[i] + 1: #Checks for ranges extended multiple digits
+                i += 1
+            if start != nums[i]: #Ranges span multiple digits
                 ranges.append(str(start) + '->' + str(nums[i]))
-            prev = nums[i]
-        if start == nums[len(nums)-1]: #In case last element wasn't handled
-            ranges.append(str(start))
+            else: #Ranges do not span multiple digits
+                ranges.append(str(start))
+            i += 1 #Continue
         return ranges

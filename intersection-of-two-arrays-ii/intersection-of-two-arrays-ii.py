9df9006ae5245652963b9fa919a6961d6e850c2f
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-09-17 03:42:52 -0700

    Cleaned up code

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -4,23 +4,13 @@ class Solution:
         Given two lists of integers, generates a list of the intersecting values
         Returns the intersection as a list
         '''
-        nums1Dict = {}
-        nums2Dict = {}
-        for num in nums1: #Hash the first list
-            if num not in nums1Dict:
-                nums1Dict[num] = 1
-            else:
-                nums1Dict[num] += 1
-        for num in nums2: #Hash the second list
-            if num not in nums2Dict:
-                nums2Dict[num] = 1
-            else:
-                nums2Dict[num] += 1
+        from collections import Counter
+        dict_nums1 = Counter(nums1)
+        dict_nums2 = Counter(nums2)
         answer = []
-        for key in nums1Dict: #Generate the intersection
-            if key in nums2Dict:
-                if nums1Dict[key] <= nums2Dict[key]:
-                    answer.extend([key] * nums1Dict[key])
-                else:
-                    answer.extend([key] * nums2Dict[key])
+        for i in dict_nums1:
+            if i in dict_nums2:
+                count_min = min(dict_nums1[i], dict_nums2[i])
+                for _ in range(count_min):
+                    answer.append(i)
         return answer

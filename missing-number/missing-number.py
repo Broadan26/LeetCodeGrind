c68b0384ff0708fd2e59bdf515e83bcab25d53ca
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-09-29 12:47:41 -0700

    Time: 136 ms (58.95%), Space: 15.5 MB (15.99%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -4,6 +4,7 @@ class Solution:
         Given a list of nums from 0 to len(nums), finds the missing num in the list
         Returns the missing num as an int
         '''
-        set_of_nums = set(nums)
-        all_possible_nums = set(range(0, len(nums)+1))
-        return all_possible_nums.difference(set_of_nums).pop()
+        xor = 0
+        for i in range(len(nums)):
+            xor ^= (i+1)^nums[i]
+        return xor

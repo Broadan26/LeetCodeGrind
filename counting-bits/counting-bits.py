87566d715bb4e6387cbb5e42446fcfa44361c982
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-09-29 12:40:44 -0700

    Time: 72 ms (97.28%), Space: 20.9 MB (34.93%) - LeetHub

--- counting-bits/counting-bits.py
+++ counting-bits/counting-bits.py
@@ -4,8 +4,7 @@ class Solution:
         Generates the number 1's for each index value of the list
         Returns the list
         '''
-        result = [0]
+        answer = [0]
         for i in range(1, n+1):
-            temp = result[i // 2] + i % 2
-            result.append(temp)
-        return result
+            answer.append(answer[i // 2] + (i % 2))
+        return answer

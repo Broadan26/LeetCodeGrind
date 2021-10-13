40648d079dfa9a053a8b987e4a27cd8bae02f67c
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-10-13 16:56:16 -0700

    More optimal solution

--- minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int: # Brute Force
         time = 0
         for point in range(0, len(points)-1):
             current = [points[point][0], points[point][1]]
@@ -29,4 +29,12 @@ class Solution:
                     current[1] -= 1
                 time += 1
                 
+        return time
+    
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        time = 0
+        for idx in range(0, len(points)-1):
+            start = points[idx]
+            end = points[idx+1]
+            time += max(abs(start[0]-end[0]), abs(start[1] - end[1]))
         return time

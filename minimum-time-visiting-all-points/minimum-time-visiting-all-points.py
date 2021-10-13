07f9cce8a1ed40ab70a86ef02ecd425dd33fd47b
Brandon <53629256+Broadan26@users.noreply.github.com>
2021-10-13 16:50:23 -0700

    Time: 2200 ms (5.07%), Space: 14.1 MB (88.15%) - LeetHub

---
+++ minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        time = 0
+        for point in range(0, len(points)-1):
+            current = [points[point][0], points[point][1]]
+            dest = [points[point+1][0], points[point+1][1]]
+            while current[0] != dest[0] and current[1] != dest[1]: #Diagonal
+                if current[0] < dest[0]:
+                    current[0] += 1
+                else:
+                    current[0] -= 1
+                if current[1] < dest[1]:
+                    current[1] += 1
+                else:
+                    current[1] -= 1
+                time += 1
+                
+            while current[0] != dest[0]: # Horizontal
+                if current[0] < dest[0]:
+                    current[0] += 1
+                else:
+                    current[0] -= 1
+                time += 1
+                    
+            while current[1] != dest[1]: # Vertical
+                if current[1] < dest[1]:
+                    current[1] += 1
+                else:
+                    current[1] -= 1
+                time += 1
+                
+        return time
